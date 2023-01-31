2fbaf29a13369470dbefa0643a4c691c539fff73
Usaid Asif <usaidasif12@gmail.com>
2023-01-31 13:00:06 +0500

    Added auto scroll,pageStorageWidget& refactor code

--- lib/features/chat/pages/chat_page.dart
+++ lib/features/chat/pages/chat_page.dart
@@ -1,9 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:custom_clippers/custom_clippers.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:intl/intl.dart';
 import 'package:whatsapp_messenger/common/extension/custom_theme_extension.dart';
 import '../../../common/helper/last_seen_message.dart';
 import '../../../common/routes/routes.dart';
@@ -12,6 +10,8 @@ import '../../../models/user_model.dart';
 import '../../auth/controller/auth_controller.dart';
 import '../controllers/chat_controller.dart';
 import '../widgets/chat_text_field.dart';
+import '../widgets/message_card.dart';
+import '../widgets/yellow_card.dart';
 
 final pageStorageBucket = PageStorageBucket();
 
@@ -138,131 +138,43 @@ class ChatPage extends ConsumerWidget {
                         child: CircularProgressIndicator(),
                       );
                     }
-                    return ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (_, index) {
-                        final message = snapshot.data![index];
-                        final isSender = message.senderId ==
-                            FirebaseAuth.instance.currentUser!.uid;
+                    return PageStorage(
+                      bucket: pageStorageBucket,
+                      child: ListView.builder(
+                        key: const PageStorageKey('chat_page_list'),
+                        itemCount: snapshot.data!.length,
+                        shrinkWrap: true,
+                        controller: scrollController,
+                        itemBuilder: (_, index) {
+                          final message = snapshot.data![index];
+                          final isSender = message.senderId ==
+                              FirebaseAuth.instance.currentUser!.uid;
 
-                        final haveNip = (index == 0) ||
-                            (index == snapshot.data!.length - 1 &&
-                                message.senderId !=
-                                    snapshot.data![index - 1].senderId) ||
-                            (message.senderId !=
-                                    snapshot.data![index - 1].senderId &&
-                                message.senderId ==
-                                    snapshot.data![index + 1].senderId) ||
-                            (message.senderId !=
-                                    snapshot.data![index - 1].senderId &&
-                                message.senderId !=
-                                    snapshot.data![index + 1].senderId);
+                          final haveNip = (index == 0) ||
+                              (index == snapshot.data!.length - 1 &&
+                                  message.senderId !=
+                                      snapshot.data![index - 1].senderId) ||
+                              (message.senderId !=
+                                      snapshot.data![index - 1].senderId &&
+                                  message.senderId ==
+                                      snapshot.data![index + 1].senderId) ||
+                              (message.senderId !=
+                                      snapshot.data![index - 1].senderId &&
+                                  message.senderId !=
+                                      snapshot.data![index + 1].senderId);
 
-                        return Column(
-                          children: [
-                            if (index == 0)
-                              Container(
-                                margin: const EdgeInsets.symmetric(
-                                  vertical: 10,
-                                  horizontal: 30,
-                                ),
-                                padding: const EdgeInsets.all(
-                                  10,
-                                ),
-                                decoration: BoxDecoration(
-                                  color: context.theme.yellowCardBgColor,
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                child: Text(
-                                  'Message and calls are end-to-end encrypted. No one outside of this chat, not even WhatsApp, can read or listen to them. Tap to learn more.',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: 13,
-                                    color: context.theme.yellowCardTextColor,
-                                  ),
-                                ),
+                          return Column(
+                            children: [
+                              if (index == 0) const YellowCard(),
+                              MessageCard(
+                                isSender: isSender,
+                                haveNip: haveNip,
+                                message: message,
                               ),
-                            Container(
-                              alignment: isSender
-                                  ? Alignment.centerRight
-                                  : Alignment.centerLeft,
-                              margin: EdgeInsets.only(
-                                top: 4,
-                                bottom: 4,
-                                left: isSender
-                                    ? 80
-                                    : haveNip
-                                        ? 10
-                                        : 15,
-                                right: isSender
-                                    ? haveNip
-                                        ? 10
-                                        : 15
-                                    : 80,
-                              ),
-                              child: ClipPath(
-                                clipper: haveNip
-                                    ? UpperNipMessageClipperTwo(
-                                        isSender
-                                            ? MessageType.send
-                                            : MessageType.receive,
-                                        nipWidth: 8,
-                                        nipHeight: 10,
-                                        bubbleRadius: haveNip ? 12 : 0,
-                                      )
-                                    : null,
-                                child: Container(
-                                  padding: EdgeInsets.only(
-                                    top: 8,
-                                    bottom: 8,
-                                    left: isSender ? 10 : 15,
-                                    right: isSender ? 15 : 10,
-                                  ),
-                                  decoration: BoxDecoration(
-                                    color: isSender
-                                        ? context.theme.senderChatCardBg
-                                        : context.theme.receiverChatCardBg,
-                                    borderRadius: haveNip
-                                        ? null
-                                        : BorderRadius.circular(12),
-                                    boxShadow: const [
-                                      BoxShadow(
-                                        color: Colors.black38,
-                                      )
-                                    ],
-                                  ),
-                                  child: Stack(
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.only(
-                                          bottom: 5,
-                                        ),
-                                        child: Text(
-                                          "${message.textMessage}         ",
-                                          style: const TextStyle(fontSize: 16),
-                                        ),
-                                      ),
-                                      Positioned(
-                                        bottom: 0,
-                                        right: 0,
-                                        child: Text(
-                                          DateFormat.Hm().format(
-                                            message.timeSent,
-                                          ),
-                                          style: TextStyle(
-                                            fontSize: 11,
-                                            color: context.theme.greyColor,
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
-                        );
-                      },
+                            ],
+                          );
+                        },
+                      ),
                     );
                   },
                 ),
